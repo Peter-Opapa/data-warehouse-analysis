60bec9f62d2e67c3104afaafd5e2c67cf4ec381b
Peter-Opapa <opapapeter82@gmail.com>
2025-06-26 11:48:07 +0300

    Create database_exploration.sql

---
+++ scripts/database_exploration.sql
@@ -0,0 +1,19 @@
+/*  
+
+-- Database Exploration
+-- Purpose: Explore database structure: list tables and inspect column metadata.
+-- Tables Used:
+--   - INFORMATION_SCHEMA.TABLES
+--   - INFORMATION_SCHEMA.COLUMNS
+
+*/
+
+USE DataWarehouse;
+--Retrieving a list of all Tables in the database
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
