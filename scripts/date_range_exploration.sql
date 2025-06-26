fd8393ea4a7632c37aa1f12de8bd7d70ad3aea79
Peter-Opapa <opapapeter82@gmail.com>
2025-06-26 15:44:22 +0300

    Update date_range_exploration.sql

--- scripts/date_range_exploration.sql
+++ scripts/date_range_exploration.sql
@@ -8,14 +8,14 @@ SQL Functions Used:
     - MIN(), MAX(), DATEDIFF()
 */
 
--- Determine the first and last order date and the total duration in months
+-- Determining the first and last order date and the total duration in months
 SELECT 
     MIN(order_date) AS first_order_date,
     MAX(order_date) AS last_order_date,
     DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
 FROM gold.fact_sales;
 
--- Find the youngest and oldest customer based on birthdate
+-- Finding the youngest and oldest customer based on birthdate
 SELECT
     MIN(birthdate) AS oldest_birthdate,
     DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
